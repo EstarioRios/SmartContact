51119a864b750f8dae8a1201529005db3ca58fc2
Abolfazl Khezri <abolfazl.khezri.business@gmail.com>
2025-08-06 00:31:25 +0330

    Yoooooooooooooooooooooooooooo Broooooooooooooooooooooo
    somthing in ContactAPP

--- Contact/models.py
+++ Contact/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from AuthenticationSystem.models import CustomUser
 
-# Create your models here.
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    phone_number = PhoneNumberField()
+    owner_user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name="contacts"
+    )
+    tags = models.ManyToManyField(
+        Tag,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
---
+++ Contact/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Tag, Contact
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = "__all__"
+
+
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
+        fields = "__all__"
